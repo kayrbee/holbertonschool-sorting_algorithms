f735093c5adcdfb455dfee2888ab3856f120f345
Katherine Beringer <github@katbee.au>
2025-08-12 01:11:22 +0000

    set up task 0

---
+++ 0-bubble_sort.c
@@ -0,0 +1,11 @@
+#include "sort.h"
+/**
+ * bubble_sort - sort integer array in asc order using bubble sort
+ * @array: array of integers
+ * @size: size of array
+ * Return: void
+ */
+void bubble_sort(int *array, size_t size)
+{
+}
+
