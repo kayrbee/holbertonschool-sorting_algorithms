554d34efa91e53e4c94b7a67427fa74121c3f8fe
Katherine Beringer <github@katbee.au>
2025-08-13 06:40:29 +0000

    wip insertion

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,6 +1,43 @@
 #include "sort.h"
 #include <stdlib.h>
 /**
+ * list_len - find the length of a list
+ * @list: pointer to list
+ * Return: integer equal to length of list
+ */
+int list_len(*list)
+{
+	int len = 0;
+	listint_t *node;
+
+	while (node != NULL)
+	{
+		len = 1;
+		node = list->next;
+	}
+	return (len);
+}
+/**
+ * insert_node - insert a node at new location
+ * @insert: pointer to node to insert
+ * @target: pointer to new location
+ * Return: void
+ */
+void insert_node(*insert, *target, **head)
+{
+	if (target->prev == NULL)
+	{
+		tmp = insert->prev;
+		insert->prev = NULL;
+		insert->next = target;
+		target->prev = insert;
+		tmp->next = NULL;
+		head = insert;
+	}
+	if (target->next == NULL)
+	{}
+}
+/**
  * insertion_sort_list - sorts a list using insertion algorithm
  * @list: list to sort
  * Return: void
@@ -19,8 +56,9 @@ void insertion_sort_list(listint_t **list)
 		while (j > 0 && list[j - 1] > tmp)
 		{
 			// swap & print result
-			insert_node(list[j], list[j - 1]);
+			insert_node(&list[j], &list[j - 1]);
 			printf("%s\n", list);
+			j--;
 		}
 		i++;
 	}
