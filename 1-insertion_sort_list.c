50888e3db313b9cc63836ec3e14b03f3cbc502f3
Katherine Beringer <github@katbee.au>
2025-09-14 06:37:26 +0000

    Add inner loop

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,65 +1,42 @@
 #include "sort.h"
 #include <stdlib.h>
 /**
- * list_len - find the length of a list
- * @list: pointer to list
- * Return: integer equal to length of list
- */
-int list_len(*list)
-{
-	int len = 0;
-	listint_t *node;
-
-	while (node != NULL)
-	{
-		len = 1;
-		node = list->next;
-	}
-	return (len);
-}
-/**
- * insert_node - insert a node at new location
- * @insert: pointer to node to insert
- * @target: pointer to new location
- * Return: void
- */
-void insert_node(*insert, *target, **head)
-{
-	if (target->prev == NULL)
-	{
-		tmp = insert->prev;
-		insert->prev = NULL;
-		insert->next = target;
-		target->prev = insert;
-		tmp->next = NULL;
-		head = insert;
-	}
-	if (target->next == NULL)
-	{}
-}
-/**
  * insertion_sort_list - sorts a list using insertion algorithm
  * @list: list to sort
  * Return: void
  */
 void insertion_sort_list(listint_t **list)
 {
-	int i = 1, j = 1;
+	listint_t *current, *tmp, *insert;
 
-	if (list == NULL)
-		exit (99);
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
 
-	while (i < list_len(list))
+	current = (*list)->next;
+	while (current != NULL)
 	{
-		tmp = list[i];
-		j = i - 1;
-		while (j >= 0 && list[j] > tmp)
+		tmp = current;
+		current = current->next;
+		insert = tmp->prev;
+		while (insert != NULL && tmp->n < insert->n)
 		{
-			list[j + 1] = list[j];
-			printf("%s\n", list);
-			j--;
+			if (tmp->next != NULL)
+			{
+				tmp->next->prev = insert;
+			}
+			insert->next = tmp->next;
+
+			tmp->prev = insert->prev;
+			tmp->next = insert;
+			insert->prev = tmp;
+
+			if (tmp->prev != NULL)
+				tmp->prev->next = tmp;
+			else
+				*list = tmp;
+			insert = tmp->prev;
 		}
-		list[j + 1] = tmp;
-		i++;
+		print_list(*list);
 	}
 }
+
---
+++ old_ex_1.c
@@ -0,0 +1,65 @@
+#include "sort.h"
+#include <stdlib.h>
+/**
+ * list_len - find the length of a list
+ * @list: pointer to list
+ * Return: integer equal to length of list
+ */
+int list_len(*list)
+{
+	int len = 0;
+	listint_t *node;
+
+	while (node != NULL)
+	{
+		len = 1;
+		node = list->next;
+	}
+	return (len);
+}
+/**
+ * insert_node - insert a node at new location
+ * @insert: pointer to node to insert
+ * @target: pointer to new location
+ * Return: void
+ */
+void insert_node(*insert, *target, **head)
+{
+	if (target->prev == NULL)
+	{
+		tmp = insert->prev;
+		insert->prev = NULL;
+		insert->next = target;
+		target->prev = insert;
+		tmp->next = NULL;
+		head = insert;
+	}
+	if (target->next == NULL)
+	{}
+}
+/**
+ * insertion_sort_list - sorts a list using insertion algorithm
+ * @list: list to sort
+ * Return: void
+ */
+void insertion_sort_list(listint_t **list)
+{
+	int i = 1, j = 1;
+
+	if (list == NULL)
+		exit (99);
+
+	while (i < list_len(list))
+	{
+		tmp = list[i];
+		j = i - 1;
+		while (j >= 0 && list[j] > tmp)
+		{
+			list[j + 1] = list[j];
+			printf("%s\n", list);
+			j--;
+		}
+		list[j + 1] = tmp;
+		i++;
+	}
+}
