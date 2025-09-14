75912488dcbeba1e4c50af3e1e443132eb785ac7
Katherine Beringer <github@katbee.au>
2025-09-14 07:17:34 +0000

    selection sort on an array

---
+++ 2-selection_sort.c
@@ -0,0 +1,34 @@
+#include "sort.h"
+#include <stdio.h>
+/**
+ *
+ */
+void selection_sort(int *array, size_t size)
+{
+	int i = 0, j = 1;
+	int min, a;
+
+	if (array == NULL || size == 0)
+		return;
+	if (size == 1)
+		return array[i];
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min_index = i;
+
+		/* Find min in array */
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min])
+				min_index = j;
+		}
+		if (min_index != i)
+		{
+			tmp = array[i];
+			array[i] = array[min_index];
+			array[min_index] = tmp;
+			print_array(array, size);
+		}
+	}
+}
